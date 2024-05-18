9482c853799c846be6021711c8a2df01f06f717b
avado@uamv.edu.ni <avado@uamv.edu.ni@>
2024-05-18 11:52:20 -0600

    nuevo

--- elbolo/PID.py
+++ elbolo/PID.py
@@ -1,24 +1,24 @@
 from pybricks.hubs import EV3Brick
-from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.ev3devices import (Motor, ColorSensor,)
 from pybricks.parameters import Port
 from pybricks.tools import wait
-import pandas as pd
+# import pandas as pd
 import time
 
 def line_follower():
     #declarar motores
-    left_motor = Motor(Port.A, positive_direction=True)
-    right_motor = Motor (Port.B, positive_direction=True)
+    left_motor = Motor(Port.C)
+    right_motor = Motor (Port.B)
     
     left_sensor = ColorSensor(Port.S1)#iniciar los sensores
     right_sensor = ColorSensor(Port.S2)
     
     #iniciar el dataframe para guardar datos
-    df = pd.DataFrame(columns=['time', 'left_light', 'right_light', 'left_speed', 'right_speed', 'error', 'turn'])
+    # df = pd.DataFrame(columns=['time', 'left_light', 'right_light', 'left_speed', 'right_speed', 'error', 'turn'])
     
     luz_negra = 15 #lo usaremos para hacer que el robot pare cuando ambos detecten menos de 15.
     speed = 200 #velocidad para los motores
-    kp = 132 #preguntar a alexander. 
+    kp = 0.12 #preguntar a alexander. 
     
     #Usar un Error para saber hacia donde se esta desviando el robot y corregir.
     
@@ -54,16 +54,17 @@ def line_follower():
         right_motor.run(speed - turn)
         
         #meter los datos en el dataframe.  
-        df = df.append({
-            'time': timestamp, 
-            'left_light': left_light,
-            'right_light': right_light,
-            'left_speed': left_motor.speed(),
-            'right_speed': right_motor.speed(),
-            'error': error,
-            'turn': turn,
-            # podemos anadir mas datos si los necesitamos. Esto es una recopilacion de datos para analisis. 
-            }, ignore_index= True) 
-        wait(100) #pausar el programa por 50ms
+        # df = df.append({
+        #     'time': timestamp, 
+        #     'left_light': left_light,
+        #     'right_light': right_light,
+        #     'left_speed': left_motor.speed(),
+        #     'right_speed': right_motor.speed(),
+        #     'error': error,
+        #     'turn': turn,
+        #     # podemos anadir mas datos si los necesitamos. Esto es una recopilacion de datos para analisis. 
+        #     }, ignore_index= True) 
+         #pausar el programa por 50ms
+        wait(25)
     
-    df.to_csv("datos_Bolo.csv", index=False)
+    # df.to_csv("datos_Bolo.csv", index=False)
--- elbolo/main.py
+++ elbolo/main.py
@@ -1,108 +1,29 @@
 #!/usr/bin/env pybricks-micropython
+import time
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, ColorSensor
-from pybricks.parameters import Port, Color
-from pybricks.tools import wait
+from pybricks.ev3devices import (Motor, ColorSensor,)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.tools import wait
+from PID import line_follower
 
-# Inicialización del brick EV3
 ev3 = EV3Brick()
-ev3.speaker.beep(1)
-ev3.screen.print("Hello UAM, Welcome to Robotics Class")
 
-# Configuración de los motores y sensores
-left_motor = Motor(Port.C)
-right_motor = Motor(Port.B)
-motor3 = Motor(Port.D)
-motor2 = Motor(Port.A)
-sensor_1 = ColorSensor(Port.S1)
-sensor_2 = ColorSensor(Port.S2)
+#Configura los motores
 
-# Inicialización del robot
-robot = DriveBase(left_motor, right_motor, 50, 50)
+# left_motor = Motor(Port.C)
+# right_motor = Motor(Port.B)
+# elveador = Motor(Port.A)
+# #Configura el robot
+# # garra = Motor(Port.D)
 
-# Función para avanzar el robot una distancia específica
-def avanzar(distancia_cm, velocidad):
-    tiempo_ms = (distancia_cm / velocidad) * 1000
-    robot.drive(velocidad, 0)
-    wait(tiempo_ms)
-    robot.stop()
+# ev3 = EV3Brick()
+# left_motor = Motor(Port.B)
+# right_motor = Motor(Port.C)
+# izquierdo = ColorSensor(Port.S1)
+# derecho = ColorSensor(Port.S2)
+# robot = DriveBase(left_motor, right_motor, 68.8, 250)
 
-# Función para girar el robot un ángulo específico
-def girar(angulo):
-    robot.turn(angulo)
-    wait(1000)
-    robot.stop()
-
-# Función para cerrar la garra
-def cerrar_garra():
-    motor3.run_angle(150, -220)
-    wait(1000)
-
-# Función para abrir la garra
-def abrir_garra():
-    motor3.run_angle(150, 220)
-    wait(1000)
-
-# Función para mover la garra hacia arriba o abajo
-def mover_garra(velocidad, angulo):
-    motor2.run(velocidad)
-    wait(angulo)
-    motor2.stop()
-
-# Función para seguir la línea con condiciones específicas para los sensores
-def seguir_linea():
-    while True:
-        color_1 = sensor_1.color
-        color_2 = sensor_2.color
-        
-        if color_1 == Color.WHITE:
-            robot.drive(20, 0)
-        else:
-            robot.stop()
-            break
-        
-        if color_2 == Color.BLACK:
-            robot.drive(20, 0)
-        else:
-            robot.stop()
-            break
-
-
-# Programa principal
-try:
-    # Seguir la línea
-    seguir_linea()
-    
-    # Avanzar una distancia específica
-    avanzar(388, 100)
-    
-    # Girar hacia la izquierda
-    girar(-175)
-    
-    # Avanzar agarrar el bloque, se avanzara poco para que no lo empuje
-    avanzar(40, 40)
-    
-    # Cierra la garra
-    cerrar_garra()
-    
-    # sube la garra, elevador, dejar en negativo por que negativo sube, positivo baja
-    mover_garra(-800, 1000)
-    
-    # Retroceder
-    robot.drive(-62, 0)
-    
-    # Girar hacia la derecha 170 grados
-    girar(170)
-    
-    # Avanzar nuevamente en linea recta
-    avanzar(300, 100)
-    
-    # Abrir la garra
-    abrir_garra()
-    
-    # Girar hacia la izquierda
-    girar(-220)
-
-except KeyboardInterrupt:
-    robot.stop()
+line_follower()
