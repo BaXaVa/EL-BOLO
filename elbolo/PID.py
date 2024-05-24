7b1e7f5d90bf66f8f0ef877d24e729412dbf8ef9
MGRILLO - Fletcher <96928501+Fletcher17b@users.noreply.github.com>
2024-05-24 07:38:57 -0600

    Pasar codigo entre branches
    Codigo de PID en Alex branch fue implementado a PID de main branch
    
    Codigo de main de Alexander branch fue implementado en main de la main branch

--- elbolo/PID.py
+++ elbolo/PID.py
@@ -1,59 +1,69 @@
-left_motor = Motor(Port.B)
-right_motor = Motor(Port.C)
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, ColorSensor,)
+from pybricks.parameters import Port
+from pybricks.tools import wait
+# import pandas as pd
+import time
 
-# Initialize the color sensor.
-line_sensor = ColorSensor(Port.S3)
+def line_follower(distancia=0):
 
+    #declarar motores
+    left_motor = Motor(Port.C)
+    right_motor = Motor (Port.B)
+    
+    left_sensor = ColorSensor(Port.S1)#iniciar los sensores
+    right_sensor = ColorSensor(Port.S2)
+    archivo = open("datos_Bolo.txt", "a")
+    
+    #iniciar el dataframe para guardar datos
+    # df = pd.DataFrame(columns=['time', 'left_light', 'right_light', 'left_speed', 'right_speed', 'error', 'turn'])
+    
+    luz_negra = 15 #lo usaremos para hacer que el robot pare cuando ambos detecten menos de 15.
+    speed = 100 #velocidad para los motores, 100mm/s
+    kp = 0.01 #preguntar a alexander. 
+    
+    #Usar un Error para saber hacia donde se esta desviando el robot y corregir.
+    
+    #Robot en la linea
+    #Si left_light = right_light, error = 0, robot esta recto
+    
+    #Robot desviado a la derecha
+    #left_light > rigth_light, error>0, robot debe girar a la izquierda
+    
+    #Robot desviado a la izquierda
+    #left_light<right_light, error<0, robot debe girar a la derecha. 
+    starTime = time.time()
+    #tiempo = speed/distancia
+    tiempo = distancia/speed
+    timeW = time.time()
+    while True:
+        #obtener valores de la luz
+        timestamp = time.time()#obtener un timestamp
+        left_light = left_sensor.reflection()
+        right_light = right_sensor.reflection()
+        if (time.time()-starTime) >= tiempo:
+            left_motor.brake()
+            right_motor.brake()
+            break
 
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
-
-BLACK = 9
-WHITE = 85
-threshold = (BLACK + WHITE) / 2
-
-
-DRIVE_SPEED = 100
-
-
-PROPORTIONAL_GAIN = 1.2
-
-
-while True:
-    # Calculate the deviation from the threshold.
-    deviation = line_sensor.reflection() - threshold
-
-    # Calculate the turn rate.
-    turn_rate = PROPORTIONAL_GAIN * deviation
-
-    # Set the drive base speed and turn rate.
-    robot.drive(DRIVE_SPEED, turn_rate)
-
-    # You can wait for a short time or do other things in this loop.
-    wait(10)
-  
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   #Configura los motores
-left_motor = Motor(Port.C)
-right_motor = Motor(Port.B)
-motor3 = Motor(Port.D)
-motor2 = Motor(Port.A)
-
-#hacer que el robot avance 38 cm
-robot = DriveBase(left_motor, right_motor, 50,50)
-distancia_avance_cm = 42
-robot.drive(500, 0)
-velocidad = 50 
-tiempo = (distancia_avance_cm / velocidad) * 1000
-
-#hacer avanzar al robot 38 cm
-wait(tiempo)
-robot.stop()
+        #si ambos sensores estan en linea negra, entonces se acabo la linea y se detiene
+        if left_light < 15 and right_light < 15:
+             left_motor.brake()
+             right_motor.brake()
+             break 
+        
+        #para calcular el error
+        error = left_light - right_light
+    
+        #calcular ajuste proporcional. Control proporcional.
+        turn = kp * error #propagacion del error. 
+    
+        #ajustar motores
+        left_motor.run(speed + turn) #speed en grados/s. 200 grados por segundo
+        right_motor.run(speed - turn)
+        timeWFinal= time.time()
+        print(timeWFinal-timeW)
+        
+    archivo.close()
+        
+    # df.to_csv("datos_Bolo.csv", index=False)
--- elbolo/main.py
+++ elbolo/main.py
@@ -21,6 +21,8 @@ sensor_2 = ColorSensor(Port.S2)
 # Inicialización del robot
 robot = DriveBase(left_motor, right_motor, 50, 50)
 
+################################################
+#Funciones Bayardo
 # Función para avanzar el robot una distancia específica
 def avanzar(distancia_cm, velocidad):
     tiempo_ms = (distancia_cm / velocidad) * 1000
@@ -68,41 +70,77 @@ def seguir_linea():
             robot.stop()
             break
 
+################################################
+#Funciones Alexander
+
+def avanzar_robot(robot, tiempo = 1):
+    tiempo *= 1000
+    robot.drive(200, 0)
+    wait(tiempo)
+    robot.stop()
+
+def retroceder_robot(robot, tiempo = 1):
+    tiempo *= 1000
+    robot.drive(-100, 0)
+    wait(tiempo)
+    robot.stop()
+def girar_180_grados(robot):
+    robot.drive(0, 90)
+    wait(892)
+    robot.stop()
+
+def girar_derecha(robot):
+    print("RIGHT")
+    robot.drive(0, 130)
+    print("RIGHT")
+    wait(7000)
+    robot.stop()
+    wait(7000)
+    print("done right")
+
+def girar_izquierda(robot):
+    robot.drive(0, -90)
+    wait(495)
+    robot.stop()
+
+def primer_paso(robot):
+    print("backing")
+    retroceder_robot(robot, 1) # El 1 representa 1 segundo
+    wait(800)
+    print("foward")
+    avanzar_robot(robot, 0.55)
+    wait(800)
+    print("turn")
+    girar_derecha(robot)
+    wait(5000)
+    print("foward")
+    avanzar_robot(robot, 0.5)
+    ev3.speaker.beep(2)
+    girar_180_grados(robot) #Gira 180 grados
+
+def main_alexander():
+    ev3.speaker.beep(1)
+    robot = DriveBase(left_motor, right_motor, 68.8,250)
+    left_motor.reset_angle(0)
+    right_motor.reset_angle(0)
+    print("paso1")
+
+    # primer_paso(robot)
+    girar_derecha(robot)
+
+#######################################################
 
 # Programa principal
 try:
-    # Seguir la línea
-    seguir_linea()
-    
-    # Avanzar una distancia específica
-    avanzar(388, 100)
-    
-    # Girar hacia la izquierda
-    girar(-175)
-    
-    # Avanzar agarrar el bloque, se avanzara poco para que no lo empuje
-    avanzar(40, 40)
-    
-    # Cierra la garra
-    cerrar_garra()
-    
-    # sube la garra, elevador, dejar en negativo por que negativo sube, positivo baja
-    mover_garra(-800, 1000)
-    
-    # Retroceder
-    robot.drive(-62, 0)
-    
-    # Girar hacia la derecha 170 grados
-    girar(170)
-    
-    # Avanzar nuevamente en linea recta
-    avanzar(300, 100)
-    
-    # Abrir la garra
-    abrir_garra()
-    
-    # Girar hacia la izquierda
-    girar(-220)
 
-except KeyboardInterrupt:
-    robot.stop()
+### Codigo Alenxader empieza
+    primer_paso(robot)
+
+
+### Codigo Alenxader termina
+
+
+except:
+    print()
+#except KeyboardInterrupt:
+#    robot.stop()
