7cf6e87031e269714cc70cbc02210922e5067cdc
avado@uamv.edu.ni <avado@uamv.edu.ni@>
2024-06-04 00:39:11 -0600

    new version

--- elbolo/main.py
+++ elbolo/main.py
@@ -23,8 +23,7 @@ giroscopio = GyroSensor(Port.S3)
 
 # Inicialización del robot
 robot = DriveBase(left_motor, right_motor, 50, 50)
-# asdakshdajs
-# adjahskdj
+
 
 ################################################
 #Funciones Bayardo
